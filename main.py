0f81a3b3342424b83c8d80deeceeb6ce0490c3c7
sennoncap <118154316+sennoncap@users.noreply.github.com>
2023-06-26 18:05:06 +0630

    Update main.py

--- main.py
+++ main.py
@@ -18,6 +18,15 @@ import src.functions.config
 import src.functions.credentials
 import src.functions.metadata
 import src.functions.tests
+from aiohttp import web
+from plugins import web_server
+
+PORT = "8080"
+
+app = web.AppRunner(await web_server())
+        await app.setup()
+        bind_address = "0.0.0.0"
+        await web.TCPSite(app, bind_address, PORT).start()
 
 colorama.init()
 print(
